d8a045ebc966dc2786d35abd96421a811fc50cd0
anubhavtech001 <anubhavtech001@gmail.com>
2025-02-19 13:06:17 +0530

    change 3d model.

--- src/components/AnubhavLab.jsx
+++ src/components/AnubhavLab.jsx
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import React, { useEffect, useRef } from "react";
-import { useGLTF, useTexture, OrbitControls } from "@react-three/drei";
+import { useGLTF, useTexture, useFrame } from "@react-three/drei";
 
 function Model(props) {
   const { nodes, materials } = useGLTF("/models/scene.glb");
@@ -34,76 +34,36 @@ function Model(props) {
           material.bumpScale = 0.1;   // More pronounced texture for leather detail
           material.normalScale = new THREE.Vector2(0.8, 0.8); // Adds more leather grain effect
           material.needsUpdate = true;
-          
         }
       }
     });
   }, [nodes, props.item]);
 
-  
+  // ✅ Rotate Model on Its Own Axis
+  useFrame(() => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += 0.01; // Smooth rotation around Y-axis
+    }
+  });
 
   return (
-    <>
-     <OrbitControls 
-  enableZoom={false} 
-  enablePan={true} 
-  enableRotate={true} 
-  maxPolarAngle={Math.PI} 
-  minPolarAngle={0} 
-  // minDistance={20}
-  // maxDistance={25}
-/>
-
-
-<group {...props} dispose={null} ref={modelRef} rotation={[0, Math.PI / 2, 0]}> {/* Default rotation 90 degrees */}
-<group position={[-0.75, 1.023, 0.055]} scale={0.148}>
-          <group position={[5.084, -6.932, -0.376]}>
-            {Object.keys(nodes).map((key, index) => (
-              nodes[key].geometry && nodes[key].material ? (
-                <mesh
-                  key={index}
-                  castShadow
-                  receiveShadow
-                  geometry={nodes[key].geometry}
-                  material={nodes[key].material}
-                />
-              ) : null
-            ))}
-          </group>
+    <group {...props} dispose={null} ref={modelRef} rotation={[0, Math.PI / 2, 0]}>
+      <group position={[-0.75, 1.023, 0.055]} scale={0.148}>
+        <group position={[5.084, -6.932, -0.376]}>
+          {Object.keys(nodes).map((key, index) => (
+            nodes[key].geometry && nodes[key].material ? (
+              <mesh
+                key={index}
+                castShadow
+                receiveShadow
+                geometry={nodes[key].geometry}
+                material={nodes[key].material}
+              />
+            ) : null
+          ))}
         </group>
-
-        {/* Branding Planes with Smaller Logo */}
-        {/* <mesh position={[0, -3, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={[0.5, 0.2, 0.5]}>
-          <planeGeometry args={[5, 2]} />
-          <meshBasicMaterial map={brandingTexture} transparent />
-        </mesh>
-
-        <mesh position={[0, 0, -3]} rotation={[0, 0, 0]} scale={[0.5, 0.2, 0.5]}>
-          <planeGeometry args={[5, 2]} />
-          <meshBasicMaterial map={brandingTexture} transparent />
-        </mesh>
-
-        <mesh position={[0, 0, 3]} rotation={[0, Math.PI, 0]} scale={[0.5, 0.2, 0.5]}>
-          <planeGeometry args={[5, 2]} />
-          <meshBasicMaterial map={brandingTexture} transparent />
-        </mesh>
-
-        <mesh position={[-3, 0, 0]} rotation={[0, Math.PI / 2, 0]} scale={[0.5, 0.2, 0.5]}>
-          <planeGeometry args={[5, 2]} />
-          <meshBasicMaterial map={brandingTexture} transparent />
-        </mesh>
-
-        <mesh position={[3, 0, 0]} rotation={[0, -Math.PI / 2, 0]} scale={[0.5, 0.2, 0.5]}>
-          <planeGeometry args={[5, 2]} />
-          <meshBasicMaterial map={brandingTexture} transparent />
-        </mesh>
-
-        <mesh position={[0, 3, 0]} rotation={[Math.PI / 2, 0, 0]} scale={[0.5, 0.2, 0.5]}>
-          <planeGeometry args={[5, 2]} />
-          <meshBasicMaterial map={brandingTexture} transparent />
-        </mesh> */}
       </group>
-    </>
+    </group>
   );
 }
 
